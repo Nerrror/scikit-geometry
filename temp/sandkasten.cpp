1cb781763aa375ebc8f5e88d5ee1e7de5b68cec9
Nerror <alexander.luce@ams-osram.com>
2023-09-05 16:10:34 +0200

    initial commit, trial and error for the polyhedra

--- temp/sandkasten.cpp
+++ temp/sandkasten.cpp
@@ -1,77 +1,88 @@
-#include <iostream>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-// #include <CGAL/Simple_cartesian.h>
-#include <CGAL/Polygon_2.h>
-#include <CGAL/Nef_polyhedron_2.h>
-
-#include <array>
-
-typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
-// typedef CGAL::Simple_cartesian<double> Kernel;
-typedef Kernel::Point_2 Point_2;
-// typedef Kernel::Vector_2 Vector_2;
-// typedef Kernel::Line_2 Line_2;
-
-typedef CGAL::Polygon_2<Kernel> Polygon2;
-typedef CGAL::Nef_polyhedron_2<Kernel> NefPoly2;
-
-int main() {
-  // Create some points
-//   Point_2 p1(0.0, 0.0, 1.0);
-//   Point_2 p2(0.0, 1.0);
-//   Point_2 p3(1.0, 0.0);
-//   Point_2 p4(1.0, 1.0);
-
-//   std::array<Point_2, 4> pointList; 
-
-//   pointList[0] = p1;
-//   pointList[1] = p2;
-//   pointList[2] = p3;
-//   pointList[3] = p4;
-  
-//   // Compute the vector between two points
-// //   Vector_2 v = p2 - p1;
-  
-//   // Compute the dot product of two vectors
-// //   double dotProduct = v * Vector_2(2.0, 1.0);
-  
-//   // Compute the length of a vector
-// //   double length = CGAL::to_double(v.squared_length());
-  
-//   // Check if three points are collinear
-// //   Point_2 p3(5.0, 7.0);
-// //   bool isCollinear = CGAL::collinear(p1, p2, p3);
-  
-//   // Create a line passing through two points
-// //   Line_2 line(p1, p2);
-
-//   Polygon2 p;
-//   NefPoly2 np;
-
-//   for (int i = 0; i<pointList.size(); i++){
-//     p.push_back(pointList[i]);
-    // np.insert(pointList[i]);
-  }
-
-
-//   np(p.vertices_begin(), p.vertices_end());
-
-//   for (auto it = p.vertices_begin(); it != p.vertices_end(); ++it) {
-//     std::cout << "(" << it->x() << ", " << it->y() << ")" << std::endl;
-//   }
-
-  for (auto it : p){
-    std::cout << "(" << it << ")" << std::endl;
-  }
-  
-  // Output the results
-//   std::cout << "Vector: " << v << std::endl;
-//   std::cout << "Dot Product: " << dotProduct << std::endl;
-//   std::cout << "Length: " << length << std::endl;
-//   std::cout << "Collinear: " << std::boolalpha << isCollinear << std::endl;
-//   std::cout << "Line: " << line << std::endl;
-
-  std::cin.get();
-  
-  return 0;
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Nef_polyhedron_3.h>
+#include <CGAL/boost/graph/convert_nef_polyhedron_to_polygon_mesh.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+typedef CGAL::Exact_predicates_exact_constructions_kernel Exact_kernel;
+typedef CGAL::Polyhedron_3<Exact_kernel> Polyhedron;
+typedef CGAL::Surface_mesh<Exact_kernel::Point_3> Surface_mesh;
+typedef CGAL::Nef_polyhedron_3<Exact_kernel> Nef_polyhedron;
+void fill_cube_1(Polyhedron &poly)
+{
+  std::string input =
+      "OFF\n\
+8 12 0\n\
+-1 -1 -1\n\
+-1 1 -1\n\
+1 1 -1\n\
+1 -1 -1\n\
+-1 -1 1\n\
+-1 1 1\n\
+1 1 1\n\
+1 -1 1\n\
+3  0 1 3\n\
+3  3 1 2\n\
+3  0 4 1\n\
+3  1 4 5\n\
+3  3 2 7\n\
+3  7 2 6\n\
+3  4 0 3\n\
+3  7 4 3\n\
+3  6 4 7\n\
+3  6 5 4\n\
+3  1 5 6\n\
+3  2 1 6";
+  std::stringstream ss;
+  ss << input;
+  ss >> poly;
+}
+void fill_cube_2(Polyhedron &poly)
+{
+  std::string input =
+      "OFF\n\
+8 12 0\n\
+-0.5 -0.5 -0.5\n\
+-0.5 0.5 -0.5\n\
+0.5 0.5 -0.5\n\
+0.5 -0.5 -0.5\n\
+-0.5 -0.5 0.5\n\
+-0.5 0.5 0.5\n\
+0.5 0.5 0.5\n\
+0.5 -0.5 0.5\n\
+3  0 1 3\n\
+3  3 1 2\n\
+3  0 4 1\n\
+3  1 4 5\n\
+3  3 2 7\n\
+3  7 2 6\n\
+3  4 0 3\n\
+3  7 4 3\n\
+3  6 4 7\n\
+3  6 5 4\n\
+3  1 5 6\n\
+3  2 1 6";
+  std::stringstream ss;
+  ss << input;
+  ss >> poly;
+}
+int main()
+{
+  // create two nested cubes
+  Polyhedron cube1, cube2;
+  fill_cube_1(cube1);
+  fill_cube_2(cube2);
+  Nef_polyhedron nef1(cube1);
+  Nef_polyhedron nef2(cube2);
+  // compute the difference of the nested cubes
+  Nef_polyhedron nef = nef1 - nef2;
+  // output the result into a Surface_mesh
+  Surface_mesh output;
+  CGAL::convert_nef_polyhedron_to_polygon_mesh(nef, output);
+  std::ofstream out;
+  out.open("out.off");
+  out << output;
+  out.close();
 }
